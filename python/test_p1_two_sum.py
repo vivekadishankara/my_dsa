ff1d04490888fd13c6bef5dbb2920ede5ec79441
Vivek Naik <vnaik@irobot.com>
2025-11-12 16:05:20 +0530

    fixed list in sol2

--- python/test_p1_two_sum.py
+++ python/test_p1_two_sum.py
@@ -1,5 +1,6 @@
 import pytest
-from typing import List
+from typing import List, Tuple
+
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -13,10 +14,10 @@ class Solution:
 
 
 @pytest.mark.parametrize("nums, target, answer",[
-    [[2,7,11,15], 9, (0,1)],
-    [[3,2,4], 6, (1,2)],
-    [[3,3], 6, (0,1)],
+    [[2,7,11,15], 9, [0,1]],
+    [[3,2,4], 6, [1,2]],
+    [[3,3], 6, [0,1]],
 ])
 def test_two_sum(nums, target, answer):
     actual_answer = Solution().twoSum(nums, target)
-    assert actual_answer == answer
+    assert list(actual_answer) == answer
